e269cced7235b8ee08c0dffa62497c812ed20ac8
minoupower554 <minoupower554@gmail.com>
2026-07-03 19:58:50 +0200

    feat: add __all__ to __init__.py

--- src/simpler_logging/__init__.py
+++ src/simpler_logging/__init__.py
@@ -2,3 +2,11 @@ from .logger import LogLevel as LogLevel, Logger as Logger, Handler as Handler
 
 logger = Logger("Main")
 """a global Logger instance for convenience."""
+
+debug = logger.debug
+info = logger.info
+warning = logger.warn
+error = logger.error
+fatal = logger.fatal
+
+__all__ = ["debug", "info", "warning", "error", "fatal", "Logger", "LogLevel", "Handler"]
